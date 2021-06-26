6112fd20a4ba96b960ccdc1304d165b6e34a45b8
Priyansh <PriyanshKulshrestha13@gmail.com>
2021-06-26 09:12:40 +0530

    day2 commit 1

--- js/script.js
+++ js/script.js
@@ -12,19 +12,31 @@ function myFunction() {
   }
 }
 
-$(document).ready(function () {
-  $(window).scroll(function () {
-    var scroll = $(window).scrollTop();
-    if (scroll > 10) {
-      $("#nav").css("background", "#9491915f");
-    } else {
-      $(".black").css("background", "black");
-    }
-  });
-});
+// $(document).ready(function () {
+//   $(window).scroll(function () {
+//     var scroll = $(window).scrollTop();
+//     if (scroll > 10) {
+//       $("#nav").css("background", "#9491915f");
+//     } else {
+//       $(".black").css("background", "black");
+//     }
+//   });
+// });
 
-var preload = document.getElementById("preloader");
-function preloader() {
-  preload.style.display = "none";
-  console.log(preload);
-}
+// var preload = document.getElementById("preloader");
+// function preloader() {
+//   preload.style.display = "none";
+//   console.log(preload);
+// }
+
+const nav = document.querySelector(".fullnav");
+const navHeight = nav.getBoundingClientRect().height;
+
+const initialCoords = nav.getBoundingClientRect();
+
+//NOTE__: Below methods by window.scrollY is not recommended bcz it slows down the apps as it fires again and again
+
+window.addEventListener("scroll", function () {
+  if (window.scrollY > navHeight) nav.classList.add("color__set");
+  else nav.classList.remove("color__set");
+});
